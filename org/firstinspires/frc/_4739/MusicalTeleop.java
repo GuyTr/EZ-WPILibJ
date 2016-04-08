e2455de6f8410a08f1daa99fc63f8dbd244afabf
GuyTr <guy@trev.es>
2016-04-08 13:41:05 +1000

    Update MusicalTeleop.java

--- org/firstinspires/frc/_4739/MusicalTeleop.java
+++ org/firstinspires/frc/_4739/MusicalTeleop.java
@@ -1,1 +1,21 @@
+package org.firstinspires.frc._4739;
 
+import org.firstinspires.frc.framework.abstraction.MatchPhaseRoutine;
+import org.firstinspires.frc._4739.JoyConfig;
+
+/**
+ * Code for the school musical.
+ * @author FRC 4739 Thunderbolts Robotics
+ */
+public class Teleop extends MatchPhaseRoutine {
+	private static final double DRIVE_SPEED_MULTIPLIER = 0.1;
+
+	public void start() {
+		//Kill auto
+		new Disabled().start();
+	}
+
+	public void tick() {
+		//TODO
+	}
+}
